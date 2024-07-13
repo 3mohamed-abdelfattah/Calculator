2e3a8e1f1675fc4fec551cc1ee8e3681a31bb6ce
MohamedMohamed <m01126165016@icloud.com>
2024-07-13 21:03:33 +0300

    Add View Binding

--- app/src/main/java/kmp/project/calculator/MainActivity.kt
+++ app/src/main/java/kmp/project/calculator/MainActivity.kt
@@ -3,18 +3,15 @@ package kmp.project.calculator
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kmp.project.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
     }
 }
